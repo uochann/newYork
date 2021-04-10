3903ff6431568a5f23ff924fed3f5dad191016fb
魚崎柾秀 <uochann@masao.local>
2021-04-10 13:49:22 +0900

    雛形作成

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('event.index');
+    }
+
+    public function create()
+    {
+        return view('event.index');
+    }
+
+    public function store()
+    {
+        return view('event.index');
+    }
+
+    public function edit()
+    {
+        return view('event.index');
+    }
+
+    public function update()
+    {
+        return view('event.index');
+    }
+
+    public function delete()
+    {
+        return view('event.index');
+    }
+
+    public function search()
+    {
+        return view('event.index');
+    }
 }
